dbf83235aafedcafc69d08f4eac626bceb56e968
limingru <18813070947@163.com>
2019-09-17 20:49:13 +0800

    s-group-button基本完成

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 import Vue from 'vue'
 import Button from './s-button.vue'
 import Icon from './s-icon.vue'
+import ButtonGroup from './s-button-group.vue'
 
 Vue.component('s-button', Button)
 Vue.component('s-icon', Icon)
+Vue.component('s-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/s-button-group.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="s-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss">
+  .s-button-group {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+    
+    > .s-button {
+      border-radius: 0;
+      
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      
+      &:nth-child(n+2) {
+        margin-left: -1px;
+      }
+      
+      /* hover的时候要让它上去 */
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
