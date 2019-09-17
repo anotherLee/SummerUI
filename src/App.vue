fdc35349172b62cd7bd7d56e1f584de031efc1c2
limingru <18813070947@163.com>
2019-09-17 11:03:08 +0800

    使用sass

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>abc</div>
+  <div id="app">
+    <Button></Button>
+  </div>
 </template>
+
+<script>
+  import Button from './s-button.vue'
+
+  export default {
+    components: {
+      Button
+    }
+  }
+</script>
+
+<style lang="scss">
+  #app {
+    margin: 20px;
+  }
+</style>
---
+++ src/s-button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="s-button">按钮</button>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss" type="text/scss">
+  .s-button {
+    height: var(--button-height);
+    padding: 0 1em;
+    font-size: var(--font-size);
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background-color: var(--button-bg);
+    
+    &:hover {
+      border-color: var(--border-color-hover);
+      cursor: var(--button-cursor-hover);
+    }
+  
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+  
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
