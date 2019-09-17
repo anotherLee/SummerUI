1de78bea931aa4d8f148b4a5fd963e007a33d304
limingru <18813070947@163.com>
2019-09-17 18:35:49 +0800

    给icon添加loading效果

--- src/s-icon.vue
+++ src/s-icon.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg v-if="name" class="icon">
+  <svg v-if="name" class="icon" :class="{ rotate: name === 'loading' }">
     <use :xlink:href="`#i-${name}`"></use>
   </svg>
 </template>
@@ -16,9 +16,17 @@
 </script>
 
 <style lang="scss">
+  @keyframes spin {
+    0% { transform: rotate(0deg) }
+    100% { transform: rotate(360deg) }
+  }
   /*这里为了和字一样高，所以用em*/
   .icon {
     width: 1em;
     height: 1em;
+  }
+  
+  .icon.rotate {
+    animation: spin 1.2s infinite linear;
   }
 </style>
