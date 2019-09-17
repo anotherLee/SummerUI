10bbff7c78f91135b1066f2349dc1b1e13a5803a
limingru <18813070947@163.com>
2019-09-17 18:04:53 +0800

    完成s-button

--- notes/Button组件.md
+++ notes/Button组件.md
@@ -1,5 +1,3 @@
-### 样式
-
 1. 按钮宽度不要写死
 
    ```css
@@ -9,3 +7,40 @@
 2. 写CSS时 `:root` 指的就是`html` 标签
 
 3. 注意区分`Vue`的两个版本：`runtime only` 和 `runtime + compiler` 版本
+
+4. 为了让icon和字一样大小，使用`em`作为单位
+
+5. 实现icon居左还是居右，**使用css和props来处理** 
+
+   ```css
+   .s-button {
+     display: inline-flex;  /* 注意这里哦 */
+     justify-content: center;
+     align-items: center;
+   }
+   /*
+   ...
+   */
+   &.icon-left {
+     .icon {
+       order: 1;
+     }
+   
+     .content {
+       order: 2
+     }
+   }
+   
+   &.icon-right {
+     .icon {
+       order: 2;
+     }
+     .content {
+       order: 1;
+     }
+   }
+   ```
+
+6. 设置了`display: inline-flex` 之后，需要`vertical-align: top` 来使多个按键对齐
+
+7. **注意props要使用驼峰写法** 
--- src/s-button.vue
+++ src/s-button.vue
@@ -1,13 +1,41 @@
+/*
+* 1. 使用slot来传文本
+*/
 <template>
-  <button class="s-button">按钮</button>
+  <button class="s-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-  export default {}
+  export default {
+    props: {
+      icon: {
+        type: String,
+        default: ''
+      },
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator: function (value) {
+          return value === 'left' || value === 'right'
+        }
+      }
+    }
+  }
 </script>
 
 <style lang="scss" type="text/scss">
   .s-button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
     height: var(--button-height);
     padding: 0 1em;
     font-size: var(--font-size);
@@ -19,13 +47,34 @@
       border-color: var(--border-color-hover);
       cursor: var(--button-cursor-hover);
     }
-  
+    
     &:active {
       background-color: var(--button-active-bg);
     }
-  
+    
     &:focus {
       outline: none;
+    }
+    
+    &.icon-left {
+      .icon {
+        order: 1;
+        margin-right: 0.3em;
+      }
+      
+      .content {
+        order: 2
+      }
+    }
+    
+    &.icon-right {
+      .icon {
+        order: 2;
+        margin-left: 0.3em;
+      }
+      .content {
+        order: 1;
+      }
     }
   }
 </style>
