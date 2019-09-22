6c2cd84e0beb5cdbe07fefff16b7407eed64a1a0
limingru <18813070947@163.com>
2019-09-22 15:21:19 +0800

    初步完成 Toast

--- src/Toast.vue
+++ src/Toast.vue
@@ -1,12 +1,51 @@
 <template>
   <div class="s-toast">
     <slot></slot>
+    <span class="close" v-if="closeButton && closeButton.text" @click="closeCallback()">
+      {{ closeButton.text }}
+    </span>
   </div>
 </template>
 
 <script>
   export default {
-    name: 's-toast'
+    name: 's-toast',
+    props: {
+      autoClose: {
+        type: Boolean,
+        default: true
+      },
+      autoCloseDelay: {
+        type: Number,
+        default: 60
+      },
+      closeButton: {
+        type: Object,
+        default: () => ({
+          text: '关闭',
+          callback: function() {
+            this.close()
+          }
+        })
+      }
+    },
+    mounted() {
+      if (this.autoClose) {
+        window.setTimeout(() => {
+          this.close()
+        }, this.autoCloseDelay * 1000)
+      }
+    },
+    methods: {
+      close() {
+        this.$el.remove()
+        this.$destroy()
+      },
+      closeCallback() {
+        this.closeButton.callback()
+        this.close()
+      }
+    }
   }
 </script>
 
@@ -19,7 +58,19 @@
     position: fixed; top: 0; left: 50%; transform: translateX(-50%);
     line-height: 1.8; height: $toast-height;
     padding: 0 1em;
-    background-color: $toast-bg;
-    font-size: $font-size;border-radius: 3px; color: #fff;
+    background-color: $toast-bg; font-size: $font-size;border-radius: 3px; color: #fff;
+    box-shadow: 0 0 2px 0 rgba(0,0,0,0.5);
+    >.close {
+      display: flex; align-items: center;
+      height: 100%; padding-left: 2em;
+      position: relative;
+      cursor: pointer;
+      &:after {
+        content: '';
+        position: absolute; left: 1em; top: 0;
+        height: 100%; width: 0;
+        border-left: 1px solid #666;
+      }
+    }
   }
 </style>
--- src/plugin.js
+++ src/plugin.js
@@ -3,7 +3,16 @@ export default {
   install(Vue, options) {
     Vue.prototype.$toast = function(msg) {
       const Constructor = Vue.extend(Toast)
-      const toast = new Constructor()
+      const toast = new Constructor({
+        propsData: {
+          closeButton: {
+            text: '知道了',
+            callback: function() {
+              console.log('用记说它知道了')
+            }
+          }
+        }
+      })
       toast.$slots.default = [msg]
       toast.$mount()
       document.body.appendChild(toast.$el)
