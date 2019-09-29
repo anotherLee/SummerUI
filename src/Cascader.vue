473d0eb3f3085b00b538dd52b24699af90e93a26
limingru <18813070947@163.com>
2019-09-29 16:15:30 +0800

    Cascader 样式基本完成

--- src/Cascader.vue
+++ src/Cascader.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="s-cascader">
     <div class="trigger" @click="popoverVisible = !popoverVisible">
+      {{ result }}
     </div>
     <div class="popover-wrapper" v-if="popoverVisible">
       <cascader-items
-        :source-items="source"
-        :height="popoverHeight"
-        :level="level"
-        :selected="selected"
-        @item-selected="onUpdateSelected"></cascader-items>
+              :source-items="source"
+              :height="popoverHeight"
+              :level="level"
+              :selected="selected"
+              @item-selected="onUpdateSelected"></cascader-items>
     </div>
   </div>
 </template>
@@ -38,11 +39,15 @@
         popoverVisible: false
       }
     },
+    computed: {
+      result() {
+        return this.selected.map(item => item.name).join('/')
+      }
+    },
     mounted() {
     },
     methods: {
       onUpdateSelected(newSelected) {
-        console.log(1)
         this.$emit('item-selected', newSelected)
       }
     },
@@ -57,14 +62,20 @@
   
   .s-cascader {
     position: relative;
+    
     .trigger {
-      border: 1px solid red;
-      height: 32px;
-      width: 120px;
+      display: inline-flex; align-items: center;
+      vertical-align: top;
+      height: $input-height;min-width: 10em;
+      padding: 0 0.5em;
+      border: 1px solid $border-color; border-radius: $border-radius;
     }
+    
     .popover-wrapper {
       position: absolute; top: 100%; left: 0;
+      margin-top: 3px;
       background-color: #fff;
+      border-radius: $border-radius;
       @extend .box-shadow;
     }
   }
