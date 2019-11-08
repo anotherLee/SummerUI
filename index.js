3069859fb961a486e9cae432364f7727da8032a8
limingru <18813070947@163.com>
2019-11-08 14:20:52 +0800

    发个新版本

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
-import Button from './src/button/Button'
-import Icon from './src/icon/Icon'
-import ButtonGroup from './src/button/ButtonGroup'
+export { default as Button } from './src/button/Button'
+export { default as ButtonGroup } from './src/button/ButtonGroup'
 
+export { default as Carousel } from './src/carousel/Carousel'
+export { default as CarouselItem } from './src/carousel/CarouselItem'
 
-export { Button, Icon, ButtonGroup }
+export { default as Cascader } from './src/cascader/Cascader'
+export { default as CascaderItems } from './src/cascader/CascaderItems'
+
+export { default as Collapse } from './src/collapse/Collapse'
+export { default as CollapseItem } from './src/collapse/CollapseItem'
+
+export { default as DatePicker } from './src/datepicker/DatePicker'
+
+export { default as Icon } from './src/icon/Icon'
+
+export { default as Input } from './src/input/Input'
+
+export { default as Layout } from './src/layout/Layout'
+export { default as Sider } from './src/layout/Sider'
+export { default as Header } from './src/layout/Header'
+export { default as Content } from './src/layout/Content'
+export { default as Footer } from './src/layout/Footer'
+
+export { default as Nav } from './src/nav/Nav'
+export { default as NavItem } from './src/nav/NavItem'
+export { default as NavSub } from './src/nav/NavSub'
+
+export { default as Pager } from './src/pager/Pager'
+
+export { default as Popover } from './src/popover/Popover'
+
+export { default as Row } from './src/row-col/Row'
+export { default as Col } from './src/row-col/Col'
+
+export { default as Scroll } from './src/scroll/Scroll'
+
+export { default as Sticky } from './src/sticky/Sticky'
+
+export { default as Table } from './src/table/Table'
+
+export { default as Tabs } from './src/tabs/Tabs'
+export { default as TabsBody } from './src/tabs/TabsBody'
+export { default as TabsHead } from './src/tabs/TabsHead'
+export { default as TabsItem } from './src/tabs/TabsItem'
+export { default as TabsPane } from './src/tabs/TabsPane'
+
+export { default as Toast } from './src/toast/plugin'
+
+export { default as Uploader } from './src/uploader/Uploader'
